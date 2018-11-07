e23a144b9876ed74aa921b9cf5b945534a934622
Kennedy12 <kennedy12@a725k>
2018-11-07 02:14:18 +0100

    Added slides used for my bachelor degree discussion

---
+++ test.pl
@@ -0,0 +1,15 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% --- EVENTS AND THEIR TYPE --- %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% PRINT WRITER JAVA
+event(a1).
+event(a2).
+event(c).
+
+of_type(T,T).
+
+test(t0, Len, Tr) :-
+	T2 = annot(c:T1, cState),
+	T1 = annot(a1:a2:T2, aState),
+	bounded_accept_all_annot(T1, Len, Tr).
